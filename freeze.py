d7353b6b6f7a5c9e79cfe368b7ccfb172d101f05
Chris Warburton <chris@localhost.(none)>
2008-06-19 21:32:31 +0100

    Got the "frozen" version working for me by including 3 more bindings
    	modified:   freeze.py
    		Added cairopango, atk and pango to be frozen
    	new file:   frozen/atk.so
    		New dependency
    	modified:   frozen/gnucleon
    		Rebuilt for included atk, pango and pangocairo
    	modified:   frozen/library.zip
    		New dependencies added
    	new file:   frozen/pango.so
    		New dependency
    	new file:   frozen/pangocairo.so
    		New dependency
    	modified:   frozen/py
    		Rebuilt

--- freeze.py
+++ freeze.py
@@ -7,7 +7,7 @@ from bbfreeze import Freezer
 # Our Freezer is given the directory we want the frozen version in and
 # a list of the Python modules we want to include (these must be
 # installed on any system running freeze.py)
-f = Freezer('frozen', includes=('gtk', 'clutter', 'cairo'))
+f = Freezer('frozen', includes=('gtk', 'clutter', 'cairo', 'pango', 'pangocairo', 'atk'))
 
 # Now we give it the Python code we want to freeze
 f.addScript('gnucleon.py')
